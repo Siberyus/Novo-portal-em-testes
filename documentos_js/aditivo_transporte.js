2641bb09e5e0bbfe18545f4e07128afe28e277d2
Siberyus <vitorbts07@gmail.com>
2026-09-16 15:03:42 -0300

    Update aditivo_transporte.js

--- documentos_js/aditivo_transporte.js
+++ documentos_js/aditivo_transporte.js
@@ -33,7 +33,7 @@ window.DocumentosAEB.gerarAditivoTransporte = function(dados) {
               { text: '_____________________________________\nPai ou Responsável\n\nNOME: ' + (!dados.isMaior ? dados.nomeResp : '') + '\nRG/CIN: ' + (!dados.isMaior ? docAssinatura : ''), style: 'assinatura' }
             ]
           },
-          { text: '_____________________________________\nAssociação dos Estudantes de Batatais', style: 'assinatura', margin: [0, 30, 0, 0] }
+          { text: '_____________________________________\nAssociação dos Estudantes de Batatais', style: 'assinatura', margin: [0, 80, 0, 0] }
         ]
       }
     ],
